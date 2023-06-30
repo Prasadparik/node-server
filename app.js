9dfb1357040d90f9efe4e966c2cb12f2b0dc8de8
Prasad Parik <prasadparik18@gmail.com>
2023-06-30 13:39:15 +0530

    added add-product route to app

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // building server ---------------------------------
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("inside middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("inside Add Product");
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="product-name"/> <input type="number" name="product-size"/> <button type="submit">Add</button> </form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("inside second middleware");
-  res.send({ key1: "value" });
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Hi There!");
+  res.send("<h1>Hi There!</h1>");
 });
 
 app.listen(3000);
